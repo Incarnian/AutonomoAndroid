c877a007ef776d27282bea389b071b74ce5dcaea
Incarnian <fillipe.bonanome@gmail.com>
2021-04-22 16:22:40 -0300

    Botões de criar clientes e voltar funcionais

--- app/src/main/java/com/cursojava/appautonomo/clients_management/ClientsActivity.java
+++ app/src/main/java/com/cursojava/appautonomo/clients_management/ClientsActivity.java
@@ -4,14 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.cursojava.appautonomo.R;
 import com.cursojava.appautonomo.adapters.ClientsAdapter;
 import com.cursojava.appautonomo.backend_request.ClientCall;
 import com.cursojava.appautonomo.backend_request.HttpClient;
 import com.cursojava.appautonomo.model.ClientResponse;
-
+import com.cursojava.appautonomo.products_management.CreateProductActivity;
 
 
 import java.util.List;
@@ -24,6 +26,8 @@ public class ClientsActivity extends AppCompatActivity {
 
     private RecyclerView clientsRecyclerView;
     private List<ClientResponse> clients;
+    private ImageView addClientButton;
+    private ImageView backClientButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +36,12 @@ public class ClientsActivity extends AppCompatActivity {
 
         clientsRecyclerView = findViewById(R.id.recycler_clients);
 
+        addClientButton = findViewById(R.id.add_client_btn);
+        addClientButton.setOnClickListener(v -> onAddNewClient());
+
+        backClientButton = findViewById(R.id.clients_back_arrow);
+        backClientButton.setOnClickListener(v -> onBackClient());
+
         ClientCall requests = HttpClient.getInstance();
 
         Call<List<ClientResponse>> clientResponse = requests.readClients();
@@ -39,7 +49,7 @@ public class ClientsActivity extends AppCompatActivity {
         clientResponse.enqueue(new Callback<List<ClientResponse>>() {
             @Override
             public void onResponse(Call<List<ClientResponse>> call, Response<List<ClientResponse>> response) {
-                if(response.isSuccessful()){
+                if (response.isSuccessful()) {
                     clients = response.body();
                     ClientsAdapter clientsAdapter = new ClientsAdapter(clients);
                     clientsRecyclerView.setAdapter(clientsAdapter);
@@ -54,7 +64,15 @@ public class ClientsActivity extends AppCompatActivity {
                 System.out.println("Alguma coisa deu errado");
             }
         });
+    }
+        private void onAddNewClient() {
+            Intent intent = new Intent(this, CreateClientActivity.class);
+            startActivity(intent);
+        }
 
+        private void onBackClient(){
+        finish();
+
+        }
 
     }
-}
